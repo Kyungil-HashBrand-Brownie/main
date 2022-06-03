218884cb41608931af47ddb881ffba0208d60ca0
seungjackie <qkrtmdwo28@gmail.com>
2022-06-03 16:10:35 +0900

    front_seung commit

--- nft_project/src/App.js
+++ nft_project/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import { Routes , Route} from 'react-router-dom';
 import HomPage from './page/HomPage';
-import Mint from './page/Mint';
+import 'bootstrap/dist/css/bootstrap.min.css';          //bootstrap 가져온것
+import Mint from './components/MintCard';
+
 
 function App() {
   return (
