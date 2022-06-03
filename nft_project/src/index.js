7f8b4f4340ebf2b840aa04c2a5055ee3a432df60
seungjackie <qkrtmdwo28@gmail.com>
2022-06-03 11:45:49 +0900

    push 1

--- nft_project/src/index.js
+++ nft_project/src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from "react-router-dom";
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
 
