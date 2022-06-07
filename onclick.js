4e164228ed9ef71d7f12efadd61f33d98523d187
sungjinWi <rkdlem94@gmail.com>
2022-06-07 14:03:11 +0900

    test

--- onclick.js
+++ onclick.js
@@ -6,17 +6,18 @@ const MintCard = () => {
     console.log(accounts)
   }
   const onClick2 = async () => {
-    await window.caver.klay.sendTransaction({
-      type: 'VALUE_TRANSFER',
-      from: window.klaytn.selectedAddress,
-      to: '0x0000000000000000000000000000000000000000',
-      value: window.caver.utils.toPeb('1', 'KLAY'),
-      gas: 8000000
-    })
+    // await window.caver.klay.sendTransaction({
+    //   type: 'VALUE_TRANSFER',
+    //   from: window.klaytn.selectedAddress,
+    //   to: '0x0000000000000000000000000000000000000000',
+    //   value: window.caver.utils.toPeb('1', 'KLAY'),
+    //   gas: 8000000
+    // })
 
-    alert("송금 성공")
+    const myContract = new window.caver.contract([{"inputs":[],"name":"getNum","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}],"0x85C6289b67E5E242fDB4546569C6FADc2b7F6222")
+    console.log(await myContract.methods.getNum().call())
 
-    // console.log( window.caver.klay.sendTransaction)
+    // alert("송금 성공")
   }
   window.klaytn.on('accountsChanged', function(accounts) {
     // Your code
