b25ac9f26576bfaa6526edca6125d30bbe50f48f
sungjinWi <rkdlem94@gmail.com>
2022-06-07 16:05:43 +0900

    feat : button mint

--- onclick.js
+++ onclick.js
@@ -1,35 +1,568 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {FreeImg} from '../img'
+import styled from "styled-components";
+import {Container,Row , Col , Button} from 'react-bootstrap'
+import ProgressBar from 'react-bootstrap/ProgressBar'
 
-const MintCard = () => {
-  const onClick = async () => {
-    const accounts = await window.klaytn.enable()
-    console.log(accounts)
-  }
-  const onClick2 = async () => {
-    // await window.caver.klay.sendTransaction({
-    //   type: 'VALUE_TRANSFER',
-    //   from: window.klaytn.selectedAddress,
-    //   to: '0x0000000000000000000000000000000000000000',
-    //   value: window.caver.utils.toPeb('1', 'KLAY'),
-    //   gas: 8000000
-    // })
 
-    const myContract = new window.caver.contract([{"inputs":[],"name":"getNum","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}],"0x85C6289b67E5E242fDB4546569C6FADc2b7F6222")
-    console.log(await myContract.methods.getNum().call())
+const StyledMain = styled.div`
+    width: 280px;
+    font-size: 1rem;
+    line-height: 1.5;
+    /* color: pink; */
+    border-radius: 0.25rem;
+    background-color: white;
+    border: 1px solid lightgray;
+    text-align: center;
+    display:flex ;
+    flex-wrap: wrap;
+    justify-content:center ;
+`;
 
-    // alert("송금 성공")
-  }
-  window.klaytn.on('accountsChanged', function(accounts) {
-    // Your code
-    console.log(accounts)
-  })
-  return (        
-    <div>
-      MintCard
-      <button onClick={onClick}>버튼</button>
-      <button onClick={onClick2}>버튼2</button>
-      </div>
+
+const StyledDiv = styled.div`
+    padding: 0.375rem 0.75rem;
+    font-size: 1rem;
+    color: green;
+    line-height: 1.5;
+    background-color: white;
+`;
+
+
+
+const StyledButton = styled.button`
+    width: 40px;
+    height: 40px;
+    padding: 0.375rem 0.75rem;
+    border-radius: 20rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    border: 1px solid lightgray;
+    color: white;
+    background-color: blue;
+`;
+
+const StyledBar = styled.div`
+    width: 200px;
+    height: 20px;
+    text-align: center;
+`;
+
+
+const FreeSale = () => {
+
+
+    const onClick = async () => {
+        const accounts = await window.klaytn.enable()
+        console.log(accounts)
+        const balance = await window.caver.klay.getBalance(accounts[0])
+        console.log(balance)
+      }
+      
+      const onClick2 = async () => {
+        // await window.caver.klay.sendTransaction({
+        //   type: 'VALUE_TRANSFER',
+        //   from: window.klaytn.selectedAddress,
+        //   to: '0x0000000000000000000000000000000000000000',
+        //   value: window.caver.utils.toPeb('1', 'KLAY'),
+        //   gas: 8000000
+        // })
+
+        const myContract = new window.caver.klay.Contract( [
+            {
+                "inputs": [],
+                "stateMutability": "nonpayable",
+                "type": "constructor"
+            },
+            {
+                "anonymous": false,
+                "inputs": [
+                    {
+                        "indexed": true,
+                        "internalType": "address",
+                        "name": "owner",
+                        "type": "address"
+                    },
+                    {
+                        "indexed": true,
+                        "internalType": "address",
+                        "name": "approved",
+                        "type": "address"
+                    },
+                    {
+                        "indexed": true,
+                        "internalType": "uint256",
+                        "name": "tokenId",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "Approval",
+                "type": "event"
+            },
+            {
+                "anonymous": false,
+                "inputs": [
+                    {
+                        "indexed": true,
+                        "internalType": "address",
+                        "name": "owner",
+                        "type": "address"
+                    },
+                    {
+                        "indexed": true,
+                        "internalType": "address",
+                        "name": "operator",
+                        "type": "address"
+                    },
+                    {
+                        "indexed": false,
+                        "internalType": "bool",
+                        "name": "approved",
+                        "type": "bool"
+                    }
+                ],
+                "name": "ApprovalForAll",
+                "type": "event"
+            },
+            {
+                "anonymous": false,
+                "inputs": [
+                    {
+                        "indexed": true,
+                        "internalType": "address",
+                        "name": "previousOwner",
+                        "type": "address"
+                    },
+                    {
+                        "indexed": true,
+                        "internalType": "address",
+                        "name": "newOwner",
+                        "type": "address"
+                    }
+                ],
+                "name": "OwnershipTransferred",
+                "type": "event"
+            },
+            {
+                "anonymous": false,
+                "inputs": [
+                    {
+                        "indexed": true,
+                        "internalType": "address",
+                        "name": "from",
+                        "type": "address"
+                    },
+                    {
+                        "indexed": true,
+                        "internalType": "address",
+                        "name": "to",
+                        "type": "address"
+                    },
+                    {
+                        "indexed": true,
+                        "internalType": "uint256",
+                        "name": "tokenId",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "Transfer",
+                "type": "event"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "address",
+                        "name": "to",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "tokenId",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "approve",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "address",
+                        "name": "owner",
+                        "type": "address"
+                    }
+                ],
+                "name": "balanceOf",
+                "outputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "",
+                        "type": "uint256"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "address",
+                        "name": "to",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "amount",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "batchMint",
+                "outputs": [],
+                "stateMutability": "payable",
+                "type": "function"
+            },
+            {
+                "inputs": [],
+                "name": "fileExtention",
+                "outputs": [
+                    {
+                        "internalType": "string",
+                        "name": "",
+                        "type": "string"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "tokenId",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "getApproved",
+                "outputs": [
+                    {
+                        "internalType": "address",
+                        "name": "",
+                        "type": "address"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "address",
+                        "name": "owner",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "address",
+                        "name": "operator",
+                        "type": "address"
+                    }
+                ],
+                "name": "isApprovedForAll",
+                "outputs": [
+                    {
+                        "internalType": "bool",
+                        "name": "",
+                        "type": "bool"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [],
+                "name": "name",
+                "outputs": [
+                    {
+                        "internalType": "string",
+                        "name": "",
+                        "type": "string"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [],
+                "name": "owner",
+                "outputs": [
+                    {
+                        "internalType": "address",
+                        "name": "",
+                        "type": "address"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "tokenId",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "ownerOf",
+                "outputs": [
+                    {
+                        "internalType": "address",
+                        "name": "",
+                        "type": "address"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [],
+                "name": "renounceOwnership",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "address",
+                        "name": "to",
+                        "type": "address"
+                    }
+                ],
+                "name": "safeMint",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "address",
+                        "name": "from",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "address",
+                        "name": "to",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "tokenId",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "safeTransferFrom",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "address",
+                        "name": "from",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "address",
+                        "name": "to",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "tokenId",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "bytes",
+                        "name": "_data",
+                        "type": "bytes"
+                    }
+                ],
+                "name": "safeTransferFrom",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "address",
+                        "name": "operator",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "bool",
+                        "name": "approved",
+                        "type": "bool"
+                    }
+                ],
+                "name": "setApprovalForAll",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "bytes4",
+                        "name": "interfaceId",
+                        "type": "bytes4"
+                    }
+                ],
+                "name": "supportsInterface",
+                "outputs": [
+                    {
+                        "internalType": "bool",
+                        "name": "",
+                        "type": "bool"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [],
+                "name": "symbol",
+                "outputs": [
+                    {
+                        "internalType": "string",
+                        "name": "",
+                        "type": "string"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "tokenId",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "tokenURI",
+                "outputs": [
+                    {
+                        "internalType": "string",
+                        "name": "",
+                        "type": "string"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "address",
+                        "name": "from",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "address",
+                        "name": "to",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "tokenId",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "transferFrom",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "address",
+                        "name": "newOwner",
+                        "type": "address"
+                    }
+                ],
+                "name": "transferOwnership",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "balance",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "withdrawKLAY",
+                "outputs": [],
+                "stateMutability": "payable",
+                "type": "function"
+            }
+        ],"0xb2dd960c8de37a5eeae785957410d58ea7ed1579")
+        await myContract.methods.batchMint(window.klaytn.selectedAddress,"1").send({from:window.klaytn.selectedAddress, gas: 300000 ,value: window.caver.utils.toPeb('1', 'KLAY')})
+        // alert("송금 성공")
+
+      }
+      
+    const [count, setCount] = useState(1)
+
+
+    const countAdd = () => {
+        setCount(count + 1);
+    }
+
+    const countMinus = () => {
+        setCount(count - 1);
+    }
+
+  return (
+    <div className="FreeDiv">
+        <StyledMain >
+            <h2>FreeSale</h2>
+            <StyledDiv >
+                <img src={FreeImg} />
+            </StyledDiv>
+            <StyledButton onClick={() => countMinus()}>  - </StyledButton>
+            <span>Mint : {count}</span>
+            <StyledButton onClick={()=> countAdd()}> +</StyledButton>
+            <Container>
+                <Row>
+                    <Col>price</Col>
+                    <Col>Per transaction</Col>
+                    <Col>Per 서기영</Col>
+                </Row>
+                <Row>
+                    <Col>60 KLAY</Col>
+                    <Col>최대 5 개</Col>
+                    <Col>limited</Col>
+                </Row>
+
+            </Container>
+            <StyledBar >
+                    <ProgressBar animated now={45} />
+            </StyledBar>
+            <br/>
+            <Button variant="success" onClick={onClick}>지갑 연결하기</Button>{' '}
+            <Button variant="success" onClick={onClick2}>000으로 1송금</Button>{' '}
+            
+        </StyledMain>
+    </div>
   )
 }
 
-export default MintCard
+export default FreeSale
