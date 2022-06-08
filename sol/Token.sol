9809730663289330d74ee745b63b9155537e51bc
Neetbear <52730691+Neetbear@users.noreply.github.com>
2022-06-08 17:34:57 +0900

    fix
    상속

--- sol/Token.sol
+++ sol/Token.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BrownieToken is ERC20, ERC20Burnable, Pausable, Ownable {
+contract BrownieToken is ERC20Burnable, Pausable, Ownable {
     constructor() ERC20("BrownieToken", "BTK") {
         _mint(msg.sender, 10000 * 10 ** decimals());
     }
