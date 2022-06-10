2f501a7ac9d70e0e3fec05f5ddf8c26a694ee159
seungjackie <qkrtmdwo28@gmail.com>
2022-06-10 12:02:05 +0900

    commit 6/10

--- front/src/page/AdminPage.js
+++ front/src/page/AdminPage.js
@@ -3,17 +3,29 @@ import QuitMint from '../components/QuitMint'
 import QuitVote from '../components/QuitVote'
 import EarthVote from '../components/EarthVote'
 import WhiteListMember from '../components/WhiteListMember'
+import { useState } from 'react'
 
 
 const AdminPage = () => {
+  const [click, setClick] = useState(false);
+
+  const click1 = () => {
+    setClick(!click)
+  }
+
   return (
       <div>
-          <WhiteListMember />
-          <QuitMint />
+        <button onClick={click1}>화이트키</button>
+        <button onClick={click1}>mint</button>
+
+        {
+          !click ? <WhiteListMember /> : <QuitMint /> 
+        }
+
           {/* <EarthVote /> */}
-          <QuitVote />
+          {/* <QuitVote /> */}
       </div>
   )
 }
 
-export default AdminPage
+export default AdminPage;
