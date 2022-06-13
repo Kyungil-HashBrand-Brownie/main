651d1eb8bf998962e9d98fa0d7241bf57526ce3d
skyshr <kiyoungsuh@naver.com>
2022-06-13 16:27:59 +0900

    feat: pfp test & swap page

--- front/src/page/Swap.js
+++ front/src/page/Swap.js
@@ -1,33 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Swap = () => {
-  return (
-    <div className='swap-box'>
-        <div className='select-box'>
-            <h2>SWAP</h2>
-            <div className='swap-select'>
-                <select className='swapL-select'>
-                    <option>선택</option>
-                    <option>BTK</option>
-                    <option>KLAY</option>
-                </select>
-                to 
-                <select className='swapR-select'>
-                    <option>선택</option>
-                    <option>BTK</option>
-                    <option>KLAY</option>
-                </select>
-            </div>
-            <div className='swap-amount-input'>
-                금액: <input /> 
-            </div>
-            <div className='swap-submit'>
-                <button 
-                // className='swap-submit'
-                type='button'>확인</button>
+    const [swap, setSwap] = useState('BTK');
+    const swapChange = (e) => {
+        if (e.target.value == 'KLAY') setSwap('BTK')
+        else setSwap('KLAY')
+    }
+    return (
+        <div className='swap-box'>
+            <div className='select-box'>
+                <h2>SWAP</h2>
+                <div className='swap-select'>
+                    <select
+                    onChange={swapChange} 
+                    className='swapL-select'
+                    >
+                        <option value='KLAY'>KLAY</option>
+                        <option value='BTK'>BTK</option>
+                    </select>
+                    to 
+                    {swap}
+                </div>
+                <div className='swap-amount-input'>
+                    금액: <input /> 
+                </div>
+                <div className='swap-submit'>
+                    <button 
+                    // className='swap-submit'
+                    type='button'>확인</button>
+                </div>
             </div>
         </div>
-    </div>
   )
 }
 
