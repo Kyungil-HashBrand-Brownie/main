21a3385cde7cc3fc0d6bbe378bfb0343175c0c2f
skyshr <kiyoungsuh@naver.com>
2022-06-13 16:29:59 +0900

    merge

--- front/src/page/Swap.js
+++ front/src/page/Swap.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react'
 
 const Swap = () => {
@@ -6,23 +5,17 @@ const Swap = () => {
     const swapChange = (e) => {
         if (e.target.value == 'KLAY') setSwap('BTK')
         else setSwap('KLAY')
-=======
-import React from 'react'
-import { useSelector } from 'react-redux'
-
-const Swap = () => {
+    }
     const {myContract} = useSelector(state =>state.nft);
     
     const swapToken = async () => {
         // await myContract.methods.instanceGetBtk(1).send({from:window.klaytn.selectedAddress, gas: 300000 ,value: window.caver.utils.toPeb(1, 'KLAY')})
->>>>>>> f120c2bd0e53cafd8d08b686debcc32d82f61202
     }
     return (
         <div className='swap-box'>
             <div className='select-box'>
                 <h2>SWAP</h2>
                 <div className='swap-select'>
-<<<<<<< HEAD
                     <select
                     onChange={swapChange} 
                     className='swapL-select'
@@ -44,32 +37,6 @@ const Swap = () => {
             </div>
         </div>
   )
-=======
-                    <select className='swapL-select'>
-                        <option>선택</option>
-                        <option>BTK</option>
-                        <option>KLAY</option>
-                    </select>
-                    to
-                    <select className='swapR-select'>
-                        <option>선택</option>
-                        <option>BTK</option>
-                        <option>KLAY</option>
-                    </select>
-                </div>
-                <div className='swap-amount-input'>
-                    금액: <input />
-                </div>
-                <div className='swap-submit'>
-                    <button
-                        // className='swap-submit'
-                        onClick={swapToken}
-                        type='button'>확인</button>
-                </div>
-            </div>
-        </div>
-    )
->>>>>>> f120c2bd0e53cafd8d08b686debcc32d82f61202
 }
 
 export default Swap
