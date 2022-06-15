7e968a70432f9c5ff4d762fd332b0badc074790f
sungjinWi <rkdlem94@gmail.com>
2022-06-15 15:57:57 +0900

    fix : gas limit extension error occurred due to gas limit

--- front/src/components/SaleFree.js
+++ front/src/components/SaleFree.js
@@ -78,7 +78,7 @@ const FreeSale = () => {
             from:myAddress, 
             to:'0xB965D7Ba9814BaF32EE004c165288365BA65eCb5',
             data:conData,
-            gas: 300000
+            gas: 3000000
         })
         console.log(test)
         alert("해당 지갑 주소로 민팅되었습니다!");
