cd11fa0853df5ea996b87aab7889d6f72eada3d5
sungjinWi <rkdlem94@gmail.com>
2022-06-23 09:52:01 +0900

    feat : minting page background

--- front/src/components/MintCard.js
+++ front/src/components/MintCard.js
@@ -2,16 +2,19 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import PreSale from './PreSale'
 import WhiteSale from './WhiteSale'
-
+import mintBackground from "img/mint/fireplace.png"
 
 const MintCard = () => {
   return (
-      <Container className='MintCard'>
+    <>
+    <img style={{position:"absolute",width:"100%", height:"100%",top:"15%", zIndex:"-2"}} src={mintBackground} />
+    <Container className='MintCard'>
         <Row>
-          <Col className='FreeSale'> <PreSale /> </Col>
-          <Col className='WhiteSale'> <WhiteSale /> </Col>
+          <Col className='FreeSale' xs={{span:4, offset:2 }}> <PreSale /> </Col>
+          <Col className='WhiteSale'xs={{span:4, }}> <WhiteSale /> </Col>
         </Row>
       </Container>
+    </>
   )
 }
 
--- front/src/page/Mint.js
+++ front/src/page/Mint.js
@@ -2,13 +2,11 @@ import React from 'react'
 import Header from '../components/Header';
 import MintCard from '../components/MintCard';
 import Profile from '../components/Profile';
-import volc from "img/mint/volcano.png"
 
 const Mint = () => {
 
   return (
     <>
-        <img style={{position:"absolute",width:"100%", height:"90%",bottom:"5%", zIndex:"-3"}} src={volc} />
       <MintCard />
       <Profile />
     </>
