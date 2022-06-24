ffe1962bb77769c2151f03e6e2b89e48f69006f0
parkisak1029 <isaaac1029@gmail.com>
2022-06-24 11:38:46 +0900

    feature:pis

---
+++ back/routes/s3.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const fs = require('fs')
+const S3 = require('aws-sdk/clients/s3')
+
+const bucketName = process.env.AWS_BUCKET_NAME
+const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRET_KEY
+
+const s3 = new S3({
+    region,
+    accessKeyId,
+    secretAccessKey
+})
+
+function getFileStream(fileKey) {
+    const downloadParams = {
+        Key: fileKey,
+        Bucket: bucketName
+    }
+    return s3.getObject(downloadParams).createReadStream()
+}
+
+exports.getFileStream = getFileStream
