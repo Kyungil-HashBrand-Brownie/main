cecce448a28dc1b628f91382b8984ab71336bed9
parkisak1029 <isaaac1029@gmail.com>
2022-06-27 12:07:53 +0900

    whitelist 설정

--- back/routes/index.js
+++ back/routes/index.js
@@ -3,51 +3,8 @@ const router = express.Router();
 const mintRouter = require('./mintRouter')
 const imageRouter = require('./imageRouter')
 
-router.use('/mint', mintRouter)
+router.use('/white', mintRouter)
+
 router.use('/image', imageRouter)
 
 module.exports = router
-
-// router.get("/admin", async (req, res) => {
-//     //db에 있는 화이트 리스트 배열로 뿌려주기
-//     console.log('admin test');
-//     const list = await pool.query(`SELECT * FROM users`)
-//     res.json(list[0])
-//     console.log(list[0])
-// })
-
-// router.post("/whitelist", async (req, res) => {
-//     //화이트리스트 db에 추가(중복검사할것)
-//     const publicKey = req.body.data.from
-//     const isValid = req.body.data.status
-//     console.log(publicKey)
-//     const publicKeys = await pool.query(`SELECT * FROM users WHERE publicKey = '${publicKey}'`)
-//     console.log(publicKeys)
-//     if (publicKeys[0].length === 0) {
-//         console.log('Not PublicKey!')
-//         console.log('----------------------')
-//         await pool.query(`INSERT INTO users(publicKey, status)VALUES('${publicKey}', '${isValid}')`)
-//         res.send('Success!')
-//     } else {
-//         console.log('PublicKey is true')
-//         console.log('----------------------')
-//         res.send('failed')
-//     }
-// })
-
-// router.post("/deletelist", async (req, res) => {
-//     //화이트리스트 db에서 삭제
-//     const publicKey = req.body.data.from
-//     const isValid = req.body.data.status
-//     if (isValid === true) {
-//         console.log('PublicKey!')
-//         await pool.query(`DELETE FROM users where publicKey = '${publicKey}'`)
-//         res.send('Success!')
-//     } else {
-//         console.log('PublicKey is false')
-//         console.log('----------------------')
-//         res.send('failed')
-//     }
-// })
-
-// module.exports = router;
--- back/routes/mintRouter/mint.control.js
+++ back/routes/mintRouter/mint.control.js
@@ -1,12 +1,17 @@
+const { json } = require('body-parser')
 const pool = require('../../db')
 
 const whitelist = async (req, res) => {
-
+    //db에 있는 화이트 리스트 배열로 뿌려주기
+    const list = await pool.query(`SELECT * FROM users`)
+    res.json(list[0])
+    console.log(list[0])
 }
 
 const whitelistPost = async (req, res) => {
-    const publicKey = req.body.data
-    console.log(publicKey)
+    const publicKey = req.body.data.from
+
+    console.log('JSON.stringify(publicKey)', publicKey)
     const publicKeys = await pool.query(`SELECT * FROM users WHERE publicKey = '${publicKey}'`)
     console.log(publicKeys)
     if (publicKeys[0].length === 0) {
@@ -22,7 +27,23 @@ const whitelistPost = async (req, res) => {
     }
 }
 
+const deletelist = async (req, res) => {
+    //화이트리스트 db에서 삭제
+    const publicKey = req.body.data.from
+    const isValid = req.body.data.status
+    if (isValid === true) {
+        console.log('PublicKey!')
+        await pool.query(`DELETE FROM users where publicKey = '${publicKey}'`)
+        res.send('Success!')
+    } else {
+        console.log('PublicKey is false')
+        console.log('----------------------')
+        res.send('failed')
+    }
+}
+
 module.exports = {
     whitelist,
-    whitelistPost
+    whitelistPost,
+    deletelist
 }
