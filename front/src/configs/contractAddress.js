3240442c9d9f14edf7a9e10e4d5226efec77373e
sungjinWi <rkdlem94@gmail.com>
2022-06-30 11:51:35 +0900

    modify : new contract with new metadata

--- front/src/configs/contractAddress.js
+++ front/src/configs/contractAddress.js
@@ -1,5 +1,5 @@
 const tokenAddr = "0x2b726Dc42028A17031D33da6D39aF7dDE7eaE25c"
-const nftAddr = "0x33D1ddB350f277A98C646482FaC03886aA595C00"
-const mintingAddr = "0xFCDCB875Bc62058B8aD4F44EE420cA3FFbbf625a"
-const votingAddr = "0xD941FCF6160E1576035544161eee07aa67F23F0B"
+const nftAddr = "0x917396A485e4AFc06280a7C4dBEc1d6F1eA80315"
+const mintingAddr = "0x0c0b5B1984Cf090f32d7C49Ec68e4DA26875d299"
+const votingAddr = "0x28c6ABF2be5a815B33F88e92eB000D2f9D1E94d5"
 export { tokenAddr, nftAddr, mintingAddr, votingAddr };
