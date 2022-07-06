b64142af0bc8dcc6a4b06b1bdb4892886bffcc60
sungjinWi <rkdlem94@gmail.com>
2022-07-06 14:12:00 +0900

    feat : added useRouter

--- back/routes/index.js
+++ back/routes/index.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const whiteRouter = require('./whiteRouter')
 const imageRouter = require('./imageRouter')
 const voteRouter = require('./voteRouter')
+const userRouter = require('./userRouter')
 
 router.use('/white', whiteRouter)
 
 router.use('/image', imageRouter)
 
 router.use('/vote', voteRouter)
+
+router.use('/user', userRouter)
 
 module.exports = router
