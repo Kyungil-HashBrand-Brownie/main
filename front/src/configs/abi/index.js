6ea2422abc75b7790f13dc3de42629d43d8410b1
Neetbear <52730691+Neetbear@users.noreply.github.com>
2022-07-06 15:03:41 +0900

    update : proxy contract
    abi, address를 proxy contract로 교체 버전

--- front/src/configs/abi/index.js
+++ front/src/configs/abi/index.js
@@ -2,10 +2,12 @@ import tokenJson from "./tokenAbi"
 import nftJson from "./nftAbi"
 import mintingJson from "./mintingAbi"
 import votingJson from "./votingAbi"
+import whitelistJson from "./whiltelistAbi"
 
 const tokenAbi = tokenJson.output.abi
 const nftAbi = nftJson.output.abi
 const mintingAbi = mintingJson.output.abi
 const votingAbi = votingJson.output.abi
+const whitelistAbi = whitelistJson.output.abi
 
-export {tokenAbi, nftAbi, mintingAbi, votingAbi}
+export {tokenAbi, nftAbi, mintingAbi, votingAbi, whitelistAbi}
--- front/src/configs/index.js
+++ front/src/configs/index.js
@@ -1,4 +1,4 @@
 export {default as caver} from "./caverjs"
-export {mintingAbi, nftAbi, tokenAbi, votingAbi} from "./abi"
-export {tokenAddr, nftAddr, mintingAddr, votingAddr} from "./contractAddress"
-export { tokenInstance, nftInstance, mintingContract, votingContract } from "./contractInstance"
+export {mintingAbi, nftAbi, tokenAbi, votingAbi, whitelistAbi} from "./abi"
+export {tokenAddr, nftAddr, mintingAddr, votingAddr, whitlistAddr} from "./contractAddress"
+export { tokenInstance, nftInstance, mintingContract, votingContract, whitelistInstance } from "./contractInstance"
