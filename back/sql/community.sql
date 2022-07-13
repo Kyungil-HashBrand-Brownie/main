98b4be07bdbfeace35c73a49b296aaeac1e63886
sungjinWi <rkdlem94@gmail.com>
2022-07-13 12:49:30 +0900

    feat : voteRead page

--- back/sql/community.sql
+++ back/sql/community.sql
@@ -4,7 +4,8 @@ CREATE TABLE voteCommunity (
     `content` VARCHAR(255) NOT NULL,
     `proposals` VARCHAR(255) NOT NULL,
     `nickname` VARCHAR(255) NOT NULL,
-    `state` VARCHAR(255) DEFAULT "승인 대기 중" NOT NULL
+    `state` VARCHAR(255) DEFAULT "승인 대기 중" NOT NULL,
+    `totalCount`INT DEFAULT 0 NULL
 );
 
 CREATE TABLE community (
