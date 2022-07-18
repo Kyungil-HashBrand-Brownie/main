3c220ae2439aa812f5c1d72362f6290879e6c7e1
Neetbear <52730691+Neetbear@users.noreply.github.com>
2022-07-18 11:00:26 +0900

    update : votingCon addr

--- front/src/configs/contractAddress.js
+++ front/src/configs/contractAddress.js
@@ -2,5 +2,5 @@ const tokenAddr = "0x9f65F9768344CDd18B61680cC2d2Bc35eFd46AF4"
 const whitelistAddr = "0xF77EcD30Dc6bC80e242e30bE5764B84B92A7c29A"
 const nftAddr = "0x41A5D6f61C80fB21660dE0840909ea710378a64D"
 const mintingAddr = "0xC29FAedcEF6617e5c417D5BC399D3331Dbf152A6"
-const votingAddr = "0x4066F5dFae347750A7a8E84295739028Ae25bffD"
+const votingAddr = "0xf311B73B49995De03F204c38Ffa7F38b5e1b4B60"
 export { tokenAddr, whitelistAddr, nftAddr, mintingAddr, votingAddr };
