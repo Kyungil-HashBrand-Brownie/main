1456523185ae9e815d40e5027b780e0de29b091f
sungjinWi <rkdlem94@gmail.com>
2022-07-20 09:19:30 +0900

    update : mint count changed

--- front/src/components/mint/MintCard.js
+++ front/src/components/mint/MintCard.js
@@ -10,14 +10,14 @@ let preSaleProps = {
   title: 'Pre-Sale',
   img: PreBrowny,
   price: 50,
-  amount: '/120',
+  amount: '/175',
 }
 
 let whiteSaleProps = {
   title: 'White-Sale',
   img: WhiteBrowny,
   price: 25,
-  amount: '/30',
+  amount: '/25',
 }
 
 const Style_Mint_div = styled.div`
