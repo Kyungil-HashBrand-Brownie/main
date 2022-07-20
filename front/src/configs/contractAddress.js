13e9f53b1976e05f17590f20aeb8981c6fa23e7a
sungjinWi <rkdlem94@gmail.com>
2022-07-20 15:02:42 +0900

    fix : voting address changed

--- front/src/configs/contractAddress.js
+++ front/src/configs/contractAddress.js
@@ -2,5 +2,5 @@ const tokenAddr = "0x052e3d7419fdA5D0d5b7B2cF72560b41c10a6784"
 const whitelistAddr = "0xE6034683Ad508045207E854289bc2FF2Fd7Eb1f2"
 const nftAddr = "0x49C3A76A1D2594EAFb85B0F07319dC9eE03ae655"
 const mintingAddr = "0x9fEDCBDCeD0fd46a4b5a30BF85bF005c07Ca4832"
-const votingAddr = "0x42874da6c9f16723A37a781E8FFe314551bc977e"
+const votingAddr = "0xb57448EB9651D42730288D0Eba4E1C5C02520844"
 export { tokenAddr, whitelistAddr, nftAddr, mintingAddr, votingAddr };
