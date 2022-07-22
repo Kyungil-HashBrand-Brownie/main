519543d55ca51f6d173c981d5e4daae43dd91adb
노진형 <52730691+Neetbear@users.noreply.github.com>
2022-07-22 14:24:16 +0900

    update : navigate
    react navigate 수정 test 후 배포해봐야 함

--- front/src/api/customHook.js
+++ front/src/api/customHook.js
@@ -5,14 +5,12 @@ const useAlert = () => {
     const navigate = useNavigate();
     const [show, setShow] = useState(false);
     const [content, setContent] = useState("")
-
+    console.log(window.location.pathname)
     const onHide = (content) =>{
       setShow(false)
-      let location = 'http://localhost:3000/write'
-      let voteLocation = 'http://localhost:3000/community/read/vote'
       if (content.includes('성공') && 
-        (window.location.href === location 
-          || window.location.href.startsWith(voteLocation))) navigate('/community')
+        (window.location.pathname === '/write'
+          || window.location.pathname === '/community/read/vote')) navigate('/community')
       else if (content === '안건이 승인되었습니다!'|| content === '투표가 종료되었습니다!' ) {
         navigate('/community')
       }
